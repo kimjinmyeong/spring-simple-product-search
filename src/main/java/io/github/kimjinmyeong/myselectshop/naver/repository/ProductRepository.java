7b1d347223083abd77dd101ab331bea3b5f0fb40
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-07-30 15:32:38 +0900

    feat: implement paging on fetching all products

--- src/main/java/io/github/kimjinmyeong/myselectshop/naver/repository/ProductRepository.java
+++ src/main/java/io/github/kimjinmyeong/myselectshop/naver/repository/ProductRepository.java
@@ -2,14 +2,14 @@ package io.github.kimjinmyeong.myselectshop.naver.repository;
 
 import io.github.kimjinmyeong.myselectshop.naver.entity.Product;
 import io.github.kimjinmyeong.myselectshop.naver.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findAllByUser(User user);
+    Page<Product> findAllByUser(User user, Pageable pageable);
 
 }
---
+++ src/main/java/io/github/kimjinmyeong/myselectshop/naver/util/TestDataRunner.java
@@ -0,0 +1,81 @@
+package io.github.kimjinmyeong.myselectshop.naver.util;
+
+import io.github.kimjinmyeong.myselectshop.naver.dto.ItemDto;
+import io.github.kimjinmyeong.myselectshop.naver.entity.Product;
+import io.github.kimjinmyeong.myselectshop.naver.entity.User;
+import io.github.kimjinmyeong.myselectshop.naver.entity.UserRoleEnum;
+import io.github.kimjinmyeong.myselectshop.naver.repository.ProductRepository;
+import io.github.kimjinmyeong.myselectshop.naver.repository.UserRepository;
+import io.github.kimjinmyeong.myselectshop.naver.service.NaverApiService;
+import io.github.kimjinmyeong.myselectshop.naver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.github.kimjinmyeong.myselectshop.naver.service.ProductService.MIN_MY_PRICE;
+
+@Component
+public class TestDataRunner implements ApplicationRunner {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    NaverApiService naverApiService;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        User testUser = new User("test", passwordEncoder.encode("1234"), "test@test.com", UserRoleEnum.USER);
+        testUser = userRepository.save(testUser);
+
+        createTestData(testUser, "Shoes");
+        createTestData(testUser, "Snacks");
+        createTestData(testUser, "Keyboard");
+        createTestData(testUser, "Tissue");
+        createTestData(testUser, "Phone");
+        createTestData(testUser, "Album");
+        createTestData(testUser, "Headphones");
+        createTestData(testUser, "Earphones");
+        createTestData(testUser, "Laptop");
+        createTestData(testUser, "Wireless Earphones");
+        createTestData(testUser, "Monitor");
+    }
+
+    private void createTestData(User user, String searchWord) {
+        List<ItemDto> itemDtoList = naverApiService.searchItems(searchWord);
+        List<Product> productList = new ArrayList<>();
+        for (ItemDto itemDto : itemDtoList) {
+            Product product = new Product();
+            product.setUser(user);
+            product.setTitle(itemDto.getTitle());
+            product.setLink(itemDto.getLink());
+            product.setImage(itemDto.getImage());
+            product.setLprice(itemDto.getLprice());
+
+            int myPrice = getRandomNumber(MIN_MY_PRICE, itemDto.getLprice() + 10000);
+            product.setMyprice(myPrice);
+
+            productList.add(product);
+        }
+
+        productRepository.saveAll(productList);
+    }
+
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+}
